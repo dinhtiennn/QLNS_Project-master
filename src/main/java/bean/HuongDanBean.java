ea8584bd6feb432a901709a574631f5e2bb8f3ac
Nessit2610 <letien26102003@gmail.com>
2024-04-09 10:33:23 +0700

    TienLaiUpdate

--- src/main/java/bean/HuongDanBean.java
+++ src/main/java/bean/HuongDanBean.java
@@ -12,6 +12,8 @@ public class HuongDanBean {
 //		3. git remote add origin https://github.com/dinhtiennn/QLNS_Project-master.git
 //		4.git pull --rebase origin master
 //		5.git add -A
-//		6.git commit -m 'messenger'
-//		7.git push  origin master
+//		6.git reset -- filename1.java filename2.java  //commit  lên trừ filename1.java filename2.java
+		// vi dụ:  git reset -- src/main/java/dao/ketnoiDB.java  //đây là DB tùy từng máy nên k commit 
+//		7.git commit -m 'messenger'
+//		8.git push  origin master
 }
