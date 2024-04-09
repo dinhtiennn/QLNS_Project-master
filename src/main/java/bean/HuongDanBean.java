cd35047dabae4049753199c1c79e39956e52c6f5
Nessit2610 <letien26102003@gmail.com>
2024-04-09 09:52:08 +0700

    messenger

---
+++ src/main/java/bean/HuongDanBean.java
@@ -0,0 +1,15 @@
+package bean;
+
+public class HuongDanBean {
+//	Các bước lấy source về:
+//		1. vào git bash 
+//		2. git init
+//		3. git remote add origin https://github.com/dinhtiennn/QLNS_Project-master.git
+//		4.git clone https://github.com/dinhtiennn/QLNS_Project-master.git
+//Các bước commit file
+//		1. vào git bash 
+//		2. git init
+//		3. git remote add origin https://github.com/dinhtiennn/QLNS_Project-master.git
+//		4.git pull --rebase origin master
+//		5.git add -A
+}
