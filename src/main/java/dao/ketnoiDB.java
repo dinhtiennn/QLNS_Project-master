ea8b4de28274b70e2462691e4ce7f2ab9963c049
TinNguyen <tinnguyen210303@gmail.com>
2024-04-16 10:55:43 +0700

    changeFile

--- src/main/java/dao/ketnoiDB.java
+++ src/main/java/dao/ketnoiDB.java
@@ -7,7 +7,7 @@ public class ketnoiDB {
 	public static Connection cn;
 	public void ketnoi() throws Exception {
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		String url ="jdbc:sqlserver://RIN\\SQLEXPRESS:1433;databaseName=QLNS; user=sa; password=123";
+		String url ="jdbc:sqlserver://LAPTOP-N2S29REU\\SQLEXPRESS03:1433;databaseName=QLNS; user=sa; password=123";
 		cn = DriverManager.getConnection(url);
 		System.out.println("Da Ket Noi");
 	}
