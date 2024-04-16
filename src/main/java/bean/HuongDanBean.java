b7e1fe283576ff10cf338f8eea503e59e39d94bf
Tin210303 <101815737+Tin210303@users.noreply.github.com>
2024-04-16 13:30:28 +0700

    Update HuongDanBean.java

--- src/main/java/bean/HuongDanBean.java
+++ src/main/java/bean/HuongDanBean.java
@@ -12,7 +12,7 @@ public class HuongDanBean {
 //		3. git remote add origin https://github.com/dinhtiennn/QLNS_Project-master.git
 //		4. git pull --rebase origin master
 //		5. git add <filename>
-//		6. git reset -- filename1.java filename2.java  //commit  lên trừ filename1.java filename2.java
+//		6.(Cần thì dùng) git reset -- filename1.java filename2.java  //commit  lên trừ filename1.java filename2.java
 		// vi dụ:  git reset -- src/main/java/dao/ketnoiDB.java  //đây là DB tùy từng máy nên k commit 
 //		7.git commit -m 'messenger'
 //		8.git push  origin master
