7a2a445df0b2a3ea4126469cf4624ae13dc3d7ee
dinhtiennn <dinhtiennguyen01@gmail.com>
2024-04-17 16:52:44 +0700

    dintien

--- src/main/java/bean/HuongDanBean.java
+++ src/main/java/bean/HuongDanBean.java
@@ -10,12 +10,10 @@ public class HuongDanBean {
 //		1. vào git bash 
 //		2. git init
 //		3. git remote add origin https://github.com/dinhtiennn/QLNS_Project-master.git
-<<<<<<< HEAD
 //		4.git pull --rebase origin master
 //		5. git add (src/main/java/dao/ketnoiDB.java)
 //		6.(Cần thì dùng) git reset -- filename1.java filename2.java  //commit  lên trừ filename1.java filename2.java
 	// vi dụ:  git reset -- src/main/java/dao/ketnoiDB.java  //đây là DB tùy từng máy nên k commit 
-=======
 //		4. git pull --rebase origin master
 //		5. git add <filename>
 //		6.(Cần thì dùng) git reset -- filename1.java filename2.java  //commit  lên trừ filename1.java filename2.java
