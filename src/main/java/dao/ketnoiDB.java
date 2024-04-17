a1f44e590f04a5a7248360e1a0d59a21e5e2ac4e
dinhtiennn <97503354+dinhtiennn@users.noreply.github.com>
2024-04-17 18:47:17 +0700

    Update ketnoiDB.java

--- src/main/java/dao/ketnoiDB.java
+++ src/main/java/dao/ketnoiDB.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-package dao;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-public class ketnoiDB {
-	public static Connection cn;
-	public void ketnoi() throws Exception {
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		String url ="jdbc:sqlserver://NESSIT\\SQLEXPRESS:1433;databaseName=QLNS; user=sa; password=123";
-		cn = DriverManager.getConnection(url);
-		System.out.println("Da Ket Noi");
-	}
-}
-=======
 package dao;
 
 import java.sql.Connection;
